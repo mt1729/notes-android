5488cab424addf52c0cbc88e314480dc65eaf736
Matthew Taylor <mt1729@gmail.com>
2024-08-16 00:21:12 -0400

    Adjust view to use newer horizontal pager, connect note count text to VM, mocked notes in repo

--- app/src/main/java/com/mt1729/notes/feature/listDetails/ListDetailsView.kt
+++ app/src/main/java/com/mt1729/notes/feature/listDetails/ListDetailsView.kt
@@ -1,15 +1,17 @@
 package com.mt1729.notes.feature.listDetails
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.ArrowBack
 import androidx.compose.material.icons.sharp.ArrowForward
@@ -19,6 +21,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,11 +32,36 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.mt1729.notes.model.Note
+import com.mt1729.notes.model.Tag
 
 @Preview
 @Composable
+fun ListDetailsPreview() {
+    ListDetailsView(
+        notes = listOf(
+            Note(
+                "Note preview", listOf(
+                    Tag("Tag preview 1"),
+                    Tag("Tag preview 2")
+                )
+            )
+        ), selectedNoteText = "1 / 24"
+    )
+}
+
+@Composable
 fun ListDetailsView(viewModel: ListDetailsViewModel = viewModel()) {
-    val notes = listOf(Note("This is the content of a note.", emptyList()))
+    val notes by viewModel.notes.collectAsState()
+    val selectedNoteText by viewModel.selectedNoteText.collectAsState()
+
+    ListDetailsView(notes, selectedNoteText)
+}
+
+// Preview-compatible overload
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ListDetailsView(notes: List<Note>, selectedNoteText: String) {
+    val notePagerState = rememberPagerState { notes.size }
 
     Column(
         modifier = Modifier
@@ -75,31 +104,32 @@ fun ListDetailsView(viewModel: ListDetailsViewModel = viewModel()) {
                 IconButton(onClick = {}) {
                     Icon(imageVector = Icons.Sharp.ArrowBack, contentDescription = "Previous note")
                 }
-                Text(modifier = Modifier.align(Alignment.CenterVertically), text = "1 / X")
+                Text(modifier = Modifier.align(Alignment.CenterVertically), text = selectedNoteText)
                 IconButton(onClick = {}) {
                     Icon(imageVector = Icons.Sharp.ArrowForward, contentDescription = "Next note")
                 }
             }
 
-            LazyRow(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                items(notes) { note ->
-                    Card(
+            HorizontalPager(
+                modifier = Modifier.fillMaxHeight(),
+                state = notePagerState
+            ) { pageIndex ->
+                val currNote = notes[pageIndex]
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp),
+                    border = BorderStroke(1.dp, Color.Black)
+                ) {
+                    Text(
                         modifier = Modifier
-                            .fillParentMaxSize(1f)
-                            .padding(8.dp),
-                        border = BorderStroke(1.dp, Color.Black)
-                    ) {
-                        Text(
-                            modifier = Modifier
-                                .padding(24.dp)
-                                .align(Alignment.Start),
-                            text = note.content,
-                            textAlign = TextAlign.Start,
-                            overflow = TextOverflow.Ellipsis
-                        )
-                    }
+                            .padding(24.dp)
+                            .align(Alignment.Start),
+                        text = currNote.content,
+                        textAlign = TextAlign.Start,
+                        overflow = TextOverflow.Ellipsis
+                    )
                 }
             }
         }
--- app/src/main/java/com/mt1729/notes/model/NoteRepository.kt
+++ app/src/main/java/com/mt1729/notes/model/NoteRepository.kt
@@ -1,3 +1,41 @@
 package com.mt1729.notes.model
 
-class NoteRepository() {}
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class NoteRepository() {
+    fun getNotes(): Flow<List<Note>> = flow {
+        val notes = listOf(
+            Note(
+                "This is the content of a short note.", listOf(
+                    Tag("Tag 1"),
+                    Tag("Tag 2"),
+                    Tag("Tag 3"),
+                    Tag("Tag 4"),
+                    Tag("Tag 5"),
+                    Tag("Tag 6"),
+
+                    )
+            ),
+            Note(
+                "QWEL QWLEJK QLWKEJ ASDL JAS POCXAJC OPIW JQWOIEJ QOW PIJACLSKC LKQJWN KJQNSCK" +
+                        "AJNSCK AJNSD QNWE IUNQWE UI@#()@#U )(@U#$ )!(U@ )I@H#O @# U$(*E@H WSC(H " +
+                        "IOUHAOIUHWDIUVSDVIU HSOI&\$Y *W&H*&H ALKSJD QWO QIWE J ALKSDJ ALSKD JQOWIE JOSIJ " +
+                        "OCIQJOCIJQOWICJQOIWJEASKC2034203948203984203984", listOf(
+                    Tag("Tag with a medium length name 1"),
+                    Tag("Tag with a medium length name 2"),
+                    Tag("Tag with a medium length name 3"),
+                    Tag("Tag with a medium length name 4")
+                )
+            ),
+            Note(
+                "Another note", listOf(
+                    Tag("Looooooooooooooooooooooooooooooooooooooooooooooooooong tag name 1"),
+                    Tag("Loooooooooooooooooooooooooooooooooooooooooooooooooooooonger tag name ")
+                )
+            )
+        )
+
+        emit(notes.shuffled())
+    }
+}
